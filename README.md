a8c75bf137ce68e7136ea4485644ac643da59d3a
Jabir Omar <jabiromar400@gmail.com>
2022-09-15 09:37:30 +0300

    Project Completed

--- README.md
+++ README.md
@@ -7,3 +7,42 @@ Here are some Features of this Webpage
 * Awesome **facts** about Animals.
 * The website is **responsive**. It's mobile, laptop and desktop friendly  
 * You can use **Dark** or **Bright theme**. 
+
+
+# Landing Page
+
+The Landing page is the first thing which will appear on screen it consist some features such as :
+
+* The Website logo.
+* Four different titles.
+* Theme changer.
+* About me.
+
+## **If you you are using a wide Screen the landing page will look like this**
+
+![alt text](images/Readme-images/landing-page.png)
+
+If you look at the left top corner you will see the **logo** 
+
+![alt text](./images/Readme-images/logo-image.png)
+
+The logo has EventListner or it scrolls the page to the top. which means if you are on bottom or in the middle and click this logo the page will **Automatically scroll to the top**.
+
+If you look top center you will see the **four titles**
+
+![alt text](./images/Readme-images/titles.png)
+
+You can click which ever you want and the page will Automatically scroll untill you reach the destination.
+
+If you look at top right corner you will see the **Theme changer** and **About me**
+
+![alt text](images/Readme-images/theme-changer.png)
+
+The moon shape icon is the Theme changer it has Click event when click that moon the whole webpage theme will be changed from **bright to dark**
+![alt text](images/Readme-images/dark-page.png)
+
+The next is **About Me**
+
+
+
+
--- web.js
+++ web.js
@@ -57,7 +57,19 @@ fetch('http://localhost:3000/right-images')
         `
         document.querySelector('.right-images').innerHTML = data1
     })
+
+    const theme = document.querySelector('#theme')
+    const body = document.querySelector('body')
+
+    theme.addEventListener('click', ()=>{
+        body.classList.toggle('dark')
+    })
+
 })
+
+
+
+
 
 fetch('http://localhost:3000/welcoming-text')
 .then((resp) => resp.json())
@@ -80,3 +92,178 @@ fetch('http://localhost:3000/welcoming-text')
 function logo(){
     window.scrollTo(0, 0)
 }
+
+
+fetch('http://localhost:3000/intro')
+.then((resp) => resp.json())
+.then((data) => {
+    let data1 = ""
+
+    data.map((value) => {
+        data1 +=`
+        <div class="intro"> <h2>${value.intro_first} <span>${value.intro_span}</span> ${value.intro_last}</h2> </div>
+        `
+
+        document.querySelector('.intro-container').innerHTML = data1
+    })
+})
+
+
+
+
+fetch('http://localhost:3000/Mammals')
+.then((resp) => resp.json())
+.then((data) => {
+    let data1 = ""
+
+    data.map((value) => {
+        data1 += `
+        <div class="animals-container">
+        <img src="${value.image}" alt="">
+
+        <div class="info">
+            <div class="info-text">
+            <br><h2>${value.name}</h2><br>
+            <p><span>Animal type</span> : ${value.animal_type}</p>
+            <p><span>Active time</span> : ${value.active_time}</p>
+            <P><span>Min-length</span> : ${value.length_min}</P>
+            <P><span>Max-length</span> : ${value.length_max}</P>
+            <p><span>Min-weight</span> : ${value.weight_min}</p>
+            <p><span>Max-weight</span> : ${value.weight_max}</p>
+            <p><span>Lifesapn</span> : ${value.life_span}</p>
+            <p><span>Habitat</span> : ${value.habitat}</p>
+            <p><span>Diet</span> : ${value.diet}</p>
+            <p><span>Geo-range</span> : ${value.geo_range}</p>
+            </div>
+        </div>
+    </div>
+        `
+
+        document.querySelector('.mammals-big-box').innerHTML = data1
+    })
+})
+
+// rebtiles start
+
+fetch('http://localhost:3000/Rebtiles')
+.then((resp) => resp.json())
+.then((data) => {
+    let data1 = ""
+
+    data.map((value) => {
+        data1 += `
+        <div class="animals-container">
+        <img src="${value.image}" alt="">
+
+        <div class="info">
+            <div class="info-text">
+            <br><h2>${value.name}</h2><br>
+            <p><span>Animal type</span> : ${value.animal_type}</p>
+            <p><span>Active time</span> : ${value.active_time}</p>
+            <P><span>Min-length</span> : ${value.length_min}</P>
+            <P><span>Max-length</span> : ${value.length_max}</P>
+            <p><span>Min-weight</span> : ${value.weight_min}</p>
+            <p><span>Max-weight</span> : ${value.weight_max}</p>
+            <p><span>Lifesapn</span> : ${value.life_span}</p>
+            <p><span>Habitat</span> : ${value.habitat}</p>
+            <p><span>Diet</span> : ${value.diet}</p>
+            <p><span>Geo-range</span> : ${value.geo_range}</p>
+            </div>
+        </div>
+    </div>
+        `
+
+        document.querySelector('.Rebtiles-big-box').innerHTML = data1
+    })
+})
+// rebtiles end
+
+// Birds start
+fetch('http://localhost:3000/Birds')
+.then((resp) => resp.json())
+.then((data) => {
+    let data1 = ""
+
+    data.map((value) => {
+        data1 += `
+        <div class="animals-container">
+        <img src="${value.image}" alt="">
+
+        <div class="info">
+            <div class="info-text">
+            <br><h2>${value.name}</h2><br>
+            <p><span>Animal type</span> : ${value.animal_type}</p>
+            <p><span>Active time</span> : ${value.active_time}</p>
+            <P><span>Min-length</span> : ${value.length_min}</P>
+            <P><span>Max-length</span> : ${value.length_max}</P>
+            <p><span>Min-weight</span> : ${value.weight_min}</p>
+            <p><span>Max-weight</span> : ${value.weight_max}</p>
+            <p><span>Lifesapn</span> : ${value.life_span}</p>
+            <p><span>Habitat</span> : ${value.habitat}</p>
+            <p><span>Diet</span> : ${value.diet}</p>
+            <p><span>Geo-range</span> : ${value.geo_range}</p>
+            </div>
+        </div>
+    </div>
+        `
+
+        document.querySelector('.birds-big-box').innerHTML = data1
+    })
+})
+// Birds end
+
+fetch('http://localhost:3000/form_box')
+.then((resp) => resp.json())
+.then((data) => {
+    let data1 = ""
+
+    data.map((value) => {
+
+        data1 = `
+        <div class="right-form">
+        <div class="join-box">
+            <div class="comment-intro">
+                <h1>${value.header}</h1><br>
+                <p>${value.intro} <br> ${value.intro2}</p><br><br>
+                <div class="phone">
+                    <img src="${value.image1}" alt="">
+                    <p>${value.phone}</p>
+                </div>
+                <div class="mail">
+                    <img src="${value.image2}" alt="">
+                    <p>${value.my_email}</p>
+                </div>
+            </div>
+            <img src="${value.manImage}" alt="">
+        </div>
+    </div>
+    <div class="left-form">
+        <div class="inputs">
+            <label for="">${value.fullName}</label>
+            <div class="input-box">
+                <input type="text" placeholder="${value.name_palceholder}" required>
+            </div>          
+        </div>
+        <div class="inputs">
+            <label for="">${value.Phone_number}</label>
+            <div class="input-box">
+                <input type="number" placeholder="${value.phone_placeholder}" required>
+            </div>
+        </div>
+        <div class="inputs">
+            <label for="">${value.email}</label>
+            <div class="input-box">
+                <input type="email" placeholder="${value.email_placeholder}" required>
+            </div>
+        </div>
+        <div id="comment-section">
+            <textarea type="comment" placeholder="${value.comment_placeholder}"></textarea>
+        </div>
+        <div id="submit-area">
+            <button id="submit" type="submit" >${value.submit}</button>
+        </div>
+    </div>
+        `
+        document.querySelector('.form-box').innerHTML = data1
+    })
+})
